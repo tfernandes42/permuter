98601e120098f70dfae9a01e08bcb7deaa8d75e6
tfernandes42 <tf4212@gmail.com>
2021-07-06 12:39:21 -0400

    version 2.0

---
+++ src/com/newsbank/permuter/types/LoggingFields.java
@@ -0,0 +1,13 @@
+package com.newsbank.permuter.types;
+
+public class LoggingFields {
+
+public final static String REQUESTTYPE = "requestType";
+public final static String REQUESTTIME = "requestTime";
+public final static String RESPONSECODE = "responseCode";
+public final static String RESPONSEMESSAGE = "responseMessage";
+public final static String CAMSAUTHID = "camsAuthId";
+
+ public static final String SAVEDALERTID = "savedAlertId";
+
+}
